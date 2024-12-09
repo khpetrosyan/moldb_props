3a57368aa63e9f2cfe6446b8f3c8ae9c946e60d5
Khoren Petrosyan <khpetrosyan@deeporigin.com>
2024-12-09 10:35:40 +0000

    new functionality

---
+++ predict_props.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import logging
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from molprops import LogS, LogP, LogD, HERG, Cyp, Ames
+from contextlib import redirect_stdout
+from io import StringIO
+
+from configs import ROOT_DIR
+
+
+logging.getLogger().setLevel(logging.ERROR)
+for logger_name in ["rdkit", "molprops", "*"]:
+    logging.getLogger(logger_name).setLevel(logging.ERROR)
+
+
+profiles = [
+    {"name": "Cyp", "model": Cyp},
+    {"name": "LogS", "model": LogS},
+    {"name": "LogP", "model": LogP},
+    {"name": "LogD", "model": LogD},
+    {"name": "HERG", "model": HERG},
+    {"name": "Ames", "model": Ames},
+]
+
+save_dir = os.path.join(ROOT_DIR, "output_props")
+os.makedirs(save_dir, exist_ok=True)
+
+for p in profiles:
+    p["save_path"] = os.path.join(save_dir, f"{p['name']}.csv")
+
+def predict_large(name, model, smiles, batch_size=640):
+    shape = (len(smiles), 5 if name == "Cyp" else 1)
+    predictions = np.full(shape=shape, fill_value=1)#np.nan)
+    
+    for i in tqdm(range(0, len(smiles), batch_size)):
+        batch = smiles[i:i+batch_size]
+        with redirect_stdout(StringIO()):
+            batch_predictions = model.predict(batch)
+        predictions[i:i+batch_size] = batch_predictions
+    return predictions.reshape(len(smiles), -1) 
+
+def main():
+    smiles_path = os.path.join(ROOT_DIR, "data/all_smiles.csv")
+    with open(smiles_path, "r") as f:
+        SMILES = f.read().split("\n")
+    
+    for p in profiles:
+        print(f"Predicting {p['name']}")
+        with redirect_stdout(StringIO()):
+            model = p["model"]()
+        
+        name = p["name"]
+        save_path = p["save_path"]  
+        
+        predictions = predict_large(name, model, SMILES)
+        df = pd.DataFrame(predictions, columns=[f"{name}_{i+1}" for i in range(predictions.shape[1])])
+        df.insert(0, "SMILES", SMILES)
+        df.to_csv(save_path, index=False)
+        print(f"Saved {name} predictions to {save_path}")
+        
+if __name__ == "__main__":
+    main()
--- scripts/complete_run.py
+++ scripts/complete_run.py
@@ -10,7 +10,100 @@ from preprocess_data import process_bindingdb_tsv
 from configs import TSV_PATH, NORMALIZED_FEAT_COLUMNS, ROOT_DIR
 
 
-class ExperimentTSNE:
+NORMALIZED_FEAT_COLUMNS = [
+    'Article_DOI',
+    'Authors',
+    'ZINC_ID_of_Ligand',
+    'koff_s-1',
+    'kon_M-1-s-1',
+    'pH',
+    'BindingDB_Entry_DOI',
+    'BindingDB_Ligand_Name',
+    'BindingDB_MonomerID',
+    'BindingDB_Reactant_set_id',
+    'ChEBI_ID_of_Ligand',
+    'ChEMBL_ID_of_Ligand',
+    'Curation_AND_DataSource',
+    'DrugBank_ID_of_Ligand',
+    'EC50_nM',
+    'IC50_nM',
+    'IUPHAR_GRAC_ID_of_Ligand',
+    'Institution',
+    'KEGG_ID_of_Ligand',
+    'Kd_nM',
+    'Ki_nM',
+    'Ligand_HET_ID_in_PDB',
+    'Ligand_InChI',
+    'Ligand_InChI_Key',
+    'Ligand_SMILES',
+    'Link_to_Ligand_in_BindingDB',
+    'Link_to_Ligand-Target_Pair_in_BindingDB',
+    'Link_to_Target_in_BindingDB',
+    'Number_of_Protein_Chains_in_Target_>1_implies_a_multichain_complex',
+    'PDB_IDs_for_Ligand-Target_Complex',
+    'PMID',
+    'Patent_Number',
+    'PubChem_AID',
+    'PubChem_CID',
+    'PubChem_SID',
+    'Target_Name',
+    'Target_Source_Organism_According_to_Curator_or_DataSource',
+    'Temp_C',
+    'PDB_IDs_of_Target_Chain',
+    'UniProt_SwissProt_Entry_Name_of_Target_Chain',
+    'UniProt_SwissProt_Primary_ID_of_Target_Chain',
+    'UniProt_TrEMBL_Submitted_Name_of_Target_Chain',
+    'UniProt_SwissProt_Recommended_Name_of_Target_Chain',
+    'UniProt_TrEMBL_Entry_Name_of_Target_Chain',
+    'UniProt_TrEMBL_Primary_ID_of_Target_Chain',
+    
+]
+
+EXTRA_COLUMNS = [
+    'BindingDB_Target_Chain_Sequence',
+    'BindingDB_Target_Chain_Sequence_NUMBER_1',
+    'BindingDB_Target_Chain_Sequence_NUMBER_2',
+    'BindingDB_Target_Chain_Sequence_NUMBER_3',
+    'PDB_IDs_of_Target_Chain_NUMBER_1',
+    'PDB_IDs_of_Target_Chain_NUMBER_2',
+    'PDB_IDs_of_Target_Chain_NUMBER_3',
+    'UniProt_SwissProt_Alternative_IDs_of_Target_Chain',
+    'UniProt_SwissProt_Alternative_IDs_of_Target_Chain_NUMBER_1',
+    'UniProt_SwissProt_Alternative_IDs_of_Target_Chain_NUMBER_2',
+    'UniProt_SwissProt_Alternative_IDs_of_Target_Chain_NUMBER_3',
+    'UniProt_SwissProt_Entry_Name_of_Target_Chain_NUMBER_1',
+    'UniProt_SwissProt_Entry_Name_of_Target_Chain_NUMBER_2',
+    'UniProt_SwissProt_Entry_Name_of_Target_Chain_NUMBER_3',
+    'UniProt_SwissProt_Primary_ID_of_Target_Chain_NUMBER_1',
+    'UniProt_SwissProt_Primary_ID_of_Target_Chain_NUMBER_2',
+    'UniProt_SwissProt_Primary_ID_of_Target_Chain_NUMBER_3',
+    'UniProt_SwissProt_Recommended_Name_of_Target_Chain_NUMBER_1',
+    'UniProt_SwissProt_Recommended_Name_of_Target_Chain_NUMBER_2',
+    'UniProt_SwissProt_Recommended_Name_of_Target_Chain_NUMBER_3',
+    'UniProt_SwissProt_Secondary_IDs_of_Target_Chain',
+    'UniProt_SwissProt_Secondary_IDs_of_Target_Chain_NUMBER_1',
+    'UniProt_SwissProt_Secondary_IDs_of_Target_Chain_NUMBER_2',
+    'UniProt_SwissProt_Secondary_IDs_of_Target_Chain_NUMBER_3',
+    'UniProt_TrEMBL_Alternative_IDs_of_Target_Chain',
+    'UniProt_TrEMBL_Alternative_IDs_of_Target_Chain_NUMBER_1',
+    'UniProt_TrEMBL_Alternative_IDs_of_Target_Chain_NUMBER_2',
+    'UniProt_TrEMBL_Alternative_IDs_of_Target_Chain_NUMBER_3',
+    'UniProt_TrEMBL_Entry_Name_of_Target_Chain_NUMBER_1',
+    'UniProt_TrEMBL_Entry_Name_of_Target_Chain_NUMBER_2',
+    'UniProt_TrEMBL_Entry_Name_of_Target_Chain_NUMBER_3',
+    'UniProt_TrEMBL_Primary_ID_of_Target_Chain_NUMBER_1',
+    'UniProt_TrEMBL_Primary_ID_of_Target_Chain_NUMBER_2',
+    'UniProt_TrEMBL_Primary_ID_of_Target_Chain_NUMBER_3',
+    'UniProt_TrEMBL_Secondary_IDs_of_Target_Chain',
+    'UniProt_TrEMBL_Secondary_IDs_of_Target_Chain_NUMBER_1',
+    'UniProt_TrEMBL_Secondary_IDs_of_Target_Chain_NUMBER_2',
+    'UniProt_TrEMBL_Secondary_IDs_of_Target_Chain_NUMBER_3',
+    'UniProt_TrEMBL_Submitted_Name_of_Target_Chain_NUMBER_1',
+    'UniProt_TrEMBL_Submitted_Name_of_Target_Chain_NUMBER_2',
+    'UniProt_TrEMBL_Submitted_Name_of_Target_Chain_NUMBER_3',
+]
+
+class ExperimentTSNEExtraCols:
     name = __qualname__
     DROPNA = True
     n_rows = 500
@@ -20,7 +113,7 @@ class ExperimentTSNE:
     N_MAX_TOKENS = 16_000
     TOKEN_BUDGET = 32_000
     EMBEDDING_ENCODING = "cl100k_base"
-    FEAT_COLUMNS = NORMALIZED_FEAT_COLUMNS
+    FEAT_COLUMNS = NORMALIZED_FEAT_COLUMNS+EXTRA_COLUMNS
     EMBEDDING_MODEL = "text-embedding-3-small"
     OUTPUTS_DIR = os.path.join(ROOT_DIR, f'output_data_{__qualname__}')
     
@@ -50,4 +143,4 @@ def main(config):
     plt.show()
     
 if __name__ == '__main__':
-    main(config=ExperimentTSNE)
+    main(config=ExperimentTSNEExtraCols)
